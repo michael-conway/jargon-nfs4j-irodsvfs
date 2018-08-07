67e434a79906cdbab36d8e189da34a80d6566747
Alek Mieczkowski <amieczko@uncc.edu>
2018-08-07 15:43:14 -0400

    asf

--- irods-vfs-impl/src/main/java/org/irods/jargon/nfs/vfs/IrodsVirtualFileSystem.java
+++ irods-vfs-impl/src/main/java/org/irods/jargon/nfs/vfs/IrodsVirtualFileSystem.java
@@ -878,7 +878,7 @@ public class IrodsVirtualFileSystem implements VirtualFileSystem
             stat.setMTime(objStat.getModifiedAt().getTime());
             
 
-            UserAO userAO = irodsAccessObjectFactory.getUserAO(acct);
+            UserAO userAO = irodsAccessObjectFactory.getUserAO(rootAccount);
             StringBuilder sb = new StringBuilder();
             sb.append(objStat.getOwnerName());
             sb.append("#");
